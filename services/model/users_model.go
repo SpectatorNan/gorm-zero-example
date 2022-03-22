806a907d98ed149a309f00db668a958762f697ac
spectatorMrZ <zhuchunan001@sina.com>
2022-03-23 00:15:33 +0800

    Update users_model.go

--- services/model/users_model.go
+++ services/model/users_model.go
@@ -57,8 +57,8 @@ func (m *defaultUsersModel) Insert(data *Users) error {
 func (m *defaultUsersModel) FindOne(id int64) (*Users, error) {
 	publicUsersIdKey := fmt.Sprintf("%s%v", cachePublicUsersIdPrefix, id)
 	var resp Users
-	err := m.QueryRow(&resp, publicUsersIdKey, func(conn *gorm.DB, v interface{}) *gorm.DB {
-		return conn.Model(&Users{}).Where("id = ?", id).First(v)
+	err := m.QueryRow(&resp, publicUsersIdKey, func(conn *gorm.DB) *gorm.DB {
+		return conn.Model(&Users{}).Where("id = ?", id)
 	})
 	switch err {
 	case nil:
@@ -69,6 +69,53 @@ func (m *defaultUsersModel) FindOne(id int64) (*Users, error) {
 		return nil, err
 	}
 }
+
+func (m *defaultUsersModel) FindOneWithoutCache(id int64) (*Users, error) {
+	var resp Users
+	err := m.QueryRowNoCache(&resp, func(conn *gorm.DB) *gorm.DB {
+		return conn.Where("id = ?", id)
+	})
+	switch err {
+	case nil:
+		return &resp, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+//func (m *defaultUsersModel) FindOneByAccount(account string) (*Users, error) {
+//	publicUsersIdKey := fmt.Sprintf("%s%v", cachePublicUsersIdPrefix, id)
+//	var resp Users
+//	err := m.QueryRow(&resp, publicUsersIdKey, func(conn *gorm.DB) *gorm.DB {
+//		return conn.Model(&Users{}).Where("id = ?", id)
+//	})
+//	switch err {
+//	case nil:
+//		return &resp, nil
+//	case gorm.ErrRecordNotFound:
+//		return nil, ErrNotFound
+//	default:
+//		return nil, err
+//	}
+//}
+//
+//func (m *defaultUsersModel) FindOne(id int64) (*Users, error) {
+//	publicUsersIdKey := fmt.Sprintf("%s%v", cachePublicUsersIdPrefix, id)
+//	var resp Users
+//	err := m.QueryRow(&resp, publicUsersIdKey, func(conn *gorm.DB) *gorm.DB {
+//		return conn.Model(&Users{}).Where("id = ?", id)
+//	})
+//	switch err {
+//	case nil:
+//		return &resp, nil
+//	case gorm.ErrRecordNotFound:
+//		return nil, ErrNotFound
+//	default:
+//		return nil, err
+//	}
+//}
 
 func (m *defaultUsersModel) Update(data *Users) error {
 	publicUsersIdKey := fmt.Sprintf("%s%v", cachePublicUsersIdPrefix, data.ID)
@@ -83,5 +130,13 @@ func (m *defaultUsersModel) Delete(id int64) error {
 	err := m.Exec(func(conn *gorm.DB) *gorm.DB {
 		return conn.Delete(&Users{}, id)
 	}, publicUsersIdKey)
+	return err
+}
+
+func (m *defaultUsersModel) DeleteWithoutCache(id int64) error {
+
+	err := m.CachedConn.ExecNoCache(func(conn *gorm.DB) *gorm.DB {
+		return conn.Delete(&Users{}, id)
+	})
 	return err
 }
